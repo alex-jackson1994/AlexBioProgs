e79c4c26fdc4f03c5eca6ec94bd5674e1d24224c
Shaun Barker <shaun.barker@student.adelaide.edu.au>
2016-01-21 10:54:51 +1030

    changes

--- PSMC_Regression/StringExtraction_ErrorAnalysis.R
+++ PSMC_Regression/StringExtraction_ErrorAnalysis.R
@@ -1,6 +1,6 @@
 library(stringr)
 
-# setwd("/home/alex/Desktop/Software/Alex/PSMC_Regression/emptydata/") # for doing tests
+setwd("~/Documents/SummerScholarship/simulatedData/regressionSimulations/") 
 #setwd("/home/alex/Desktop/Simulations/Regression/")
 
 filelist = list.files(pattern = "*.txt",recursive=T)
@@ -44,6 +44,11 @@ msFile_eN_2_vec = function(msFilePath, t_or_p) {
 #FauxHumanPops = 4*FauxHumanTimesPops[c(FALSE, TRUE)] # extracts the even values (i.e. the pops) and scales
 
 # need to define true times and populations
+#msFilelist = list.files(pattern = "*ms",recursive=T)
+#for(msFile in msFilelist){
+#  
+#}
+
 fauxHumanMs_eN = "-eN 0.0055 0.0832 -eN 0.0089 0.0489 -eN 0.0130 0.0607 -eN 0.0177 0.1072 -eN 0.0233 0.2093 -eN 0.0299 0.3630 -eN 0.0375 0.5041 -eN 0.0465 0.5870 -eN 0.0571 0.6343 -eN 0.0695 0.6138 -eN 0.0840 0.5292 -eN 0.1010 0.4409 -eN 0.1210 0.3749 -eN 0.1444 0.3313 -eN 0.1718 0.3066 -eN 0.2040 0.2952 -eN 0.2418 0.2915 -eN 0.2860 0.2950 -eN 0.3379 0.3103 -eN 0.3988 0.3458 -eN 0.4701 0.4109 -eN 0.5538 0.5048 -eN 0.6520 0.5996 -eN 0.7671 0.6440 -eN 0.9020 0.6178 -eN 1.0603 0.5345 -eN 1.4635 1.7931"
 fauxHumanTimes = ms_eN_2_vec(fauxHumanMs_eN,TRUE)
 fauxHumanPops = ms_eN_2_vec(fauxHumanMs_eN,FALSE)
@@ -52,7 +57,7 @@ constantPopPops = 4*rep(1,length(constantPopTimes))
 psmcSim1MS_eN = "-eN 0.01 0.1 -eN 0.06 1 -eN 0.2 0.5 -eN 1 1 -eN 2 2"
 psmcSim1Times = ms_eN_2_vec(psmcSim1MS_eN,TRUE)
 psmcSim1Pops = ms_eN_2_vec(psmcSim1MS_eN,FALSE)
-psmcSim2MS_eN = "-eN 0.1 5 -eN 0.6 20 -eN 2 5 -eN 10 10 -eN 20 5"
+psmcSim2MS_eN = "-eN 0.1 1 -eN 0.6 4 -eN 1 1 -eN 3 2 -eN 5 1"
 psmcSim2Times = ms_eN_2_vec(psmcSim2MS_eN,TRUE)
 psmcSim2Pops = ms_eN_2_vec(psmcSim2MS_eN,FALSE)
 trench_Ms_eN = "-eN 0.01 1 -eN 0.1 0.8 -eN 0.2 0.6 -eN 0.3 0.4 -eN 0.4 0.3 -eN 0.5 0.25 -eN 0.6 0.2 -eN 0.7 0.3 -eN 0.8 0.4 -eN 0.9 0.6 -eN 1 0.8 -eN 1.1 1"
@@ -74,8 +79,8 @@ for (infile in filelist) {
   logmaxs[i] = log(max(data.infile$t_k/2))
   i = i+1
 }
-logmins[length(filelist)+1] = log(min(fauxHumanTimes,constantPopTimes,psmcSim2Times)) # add true value min
-logmaxs[length(filelist)+1] = log(max(fauxHumanTimes,constantPopTimes,psmcSim2Times)) # add true value max
+logmins[length(filelist)+1] = log(max(min(fauxHumanTimes),min(constantPopTimes),min(psmcSim2Times),min(psmcSim1Times),min(trenchTimes))) # add true value min
+logmaxs[length(filelist)+1] = log(min(max(fauxHumanTimes),max(constantPopTimes),max(psmcSim2Times),max(psmcSim1Times),max(trenchTimes))) # add true value max
 logmax_of_mins = max(logmins)
 logmin_of_maxs = min(logmaxs)
 
@@ -83,6 +88,12 @@ logmin_of_maxs = min(logmaxs)
 # this function will tell you PSMC's population size estimate at time "pos" given the x (time) and y (relative pop) data
 eval_popsize = function(pos, x, y){ 
   xIndex = length(which(x <= pos))
+  if(xIndex==0){
+    print(pos)
+    print(x)
+    print(y)
+    return(NaN)
+  }
   return(y[xIndex])
 }
 
@@ -139,27 +150,9 @@ for (infile in filelist) { # make sure I change things to Shaun's file format
   
   # error analysis
   data = read.table(infile,header=TRUE)
-  # need to make sure you integrate against the correct true curve
-  if (Pop_Dynamics_Type_tmp == "fauxHuman") {
-    ErrorVal = integrate(absdifference,logmax_of_mins,logmin_of_maxs, d1=log(fauxHumanTimes), d2=fauxHumanPops, d3=log(data$t_k/2), d4=data$lambda_k, subdivisions=10000)$value 
-    Error = append(Error,ErrorVal)
-  }
-  if (Pop_Dynamics_Type_tmp == "constantPop") {
-    Error = integrate(absdifference,logmax_of_mins,logmin_of_maxs, d1=log(constantPopTimes), d2=constantPopPops, d3=log(data$t_k/2), d4=data$lambda_k, subdivisions=10000)$value 
-    Error = append(Error,ErrorVal)
-  } 
-  if (Pop_Dynamics_Type_tmp == "psmcSim2") {
-    Error = integrate(absdifference,logmax_of_mins,logmin_of_maxs, d1=log(psmcSim2Times), d2=psmcSim2Pops, d3=log(data$t_k/2), d4=data$lambda_k, subdivisions=10000)$value
-    Error = append(Error,ErrorVal)
-  }
-  if (Pop_Dynamics_Type_tmp == "psmcSim1") {
-    Error = integrate(absdifference,logmax_of_mins,logmin_of_maxs, d1=log(psmcSim1Times), d2=psmcSim1Pops, d3=log(data$t_k/2), d4=data$lambda_k, subdivisions=10000)$value
-    Error = append(Error,ErrorVal)
-  }
-  if (Pop_Dynamics_Type_tmp == "trench") {
-    Error = integrate(absdifference,logmax_of_mins,logmin_of_maxs, d1=log(trenchTimes), d2=trenchPops, d3=log(data$t_k/2), d4=data$lambda_k, subdivisions=10000)$value
-    Error = append(Error,ErrorVal)
-  }
+  #simplified if statements to concatenating Pop_Dynamics_Type_tmp (which should hopefully contain the name of the sim we're running)
+  ErrorVal = integrate(absdifference, logmax_of_mins, logmin_of_maxs, d1=log(get(paste(Pop_Dynamics_Type_tmp,'Times',sep=''))), d2=get(paste(Pop_Dynamics_Type_tmp,'Pops',sep='')), d3=log(data$t_k/2), d4=data$lambda_k, subdivisions=10000)$value 
+  Error = append(Error,ErrorVal)
 } 
 Results = data.frame(Bp,Int,Split,Bp_per_contig,Per_Site_Mut_Rate,Per_Site_Recomb_Rate,Pop_Dynamics_Type,Error)
 
