7e41d9351a4896f8119d21c8810a336b8c6e0ecd
Alex Jackson <alex.jackson@student.adelaide.edu.au>
2016-02-04 15:50:58 +1030

    Changed some things in red kangaroo plots in attempt to recover pop dynamics in years

--- redKangaroo/redKangarooRPlots.R
+++ redKangaroo/redKangarooRPlots.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 
 # the redKangarooInt4+5*3+4.psmc run
-kanga1 = read.delim("/media/alex/My Passport/bioinformaticsScholarship/Data/redKangarooInt4+5*3+4.txt")
+kanga1 <- read.delim("~/Desktop/Data/redKangaroo/redKangarooInt4+5*3+4.txt")
 # the redKangarooInt40.psmc run
 # THIS ONE JUST GAVE CONSTANT POP
 # kanga2 = read.delim("/media/alex/My Passport/bioinformaticsScholarship/Data/redKangarooInt40.txt")
@@ -10,7 +10,7 @@ kanga1 = read.delim("/media/alex/My Passport/bioinformaticsScholarship/Data/redK
 # recall that time (x) must be divided by 2 to give in terms on N_0 generations having past
 # taking log of time for clarity
 ggplot(data=kanga1, mapping=aes(x = log(t_k/2), y = lambda_k)) + geom_step(colour="red") + labs(xlab("log(t_k/2) (log N_0 generations)"), ylab("lambda_k")) + ggtitle("History of the red kangaroo's population dynamics") # + geom_step(data = kanga2, mapping=aes(x=log(t_k/2), y=lambda_k), colour="black")
-ggplot(data=kanga1, mapping=aes(x = t_k/2, y = lambda_k)) + geom_step(colour="red") + labs(xlab("t_k/2 (N_0 generations)"), ylab("lambda_k")) + ggtitle("History of the red kangaroo's population dynamics")
+#ggplot(data=kanga1, mapping=aes(x = t_k/2, y = lambda_k)) + geom_step(colour="red") + labs(xlab("t_k/2 (N_0 generations)"), ylab("lambda_k")) + ggtitle("History of the red kangaroo's population dynamics")
 
 ### Can we work out when back in time this was? NOTE: STRONG POSSIBILITY THIS IS COMPLETELY WRONG
 
@@ -21,14 +21,16 @@ N_0 = ceiling(11514298/2) # I haven't found an upper and lower estimate at the m
 gen_low = 7
 gen_hi = 10
 
-# let's unscale time. At the moment, 1 unit of scaled time is equal to N_0 generations having passed.
-kanga1$t_unscaled_low = kanga1$t_k/2 * N_0 * gen_low
-kanga1$t_unscaled_hi = kanga1$t_k/2 * N_0 * gen_hi
+# let's unscale time to years. At the moment, 1 unit of scaled time is equal to 2N_0 generations having passed.
+kanga1$t_unscaled_low = kanga1$t_k*2 * N_0 * gen_low
+kanga1$t_unscaled_hi = kanga1$t_k*2 * N_0 * gen_hi
 kanga1$pop_hist = kanga1$lambda_k * N_0
 
 # when was the LGM... according to wikipedia lol,
 LGM_start = 26500
 LGM_end = 19000
 
-# let's see if the population dynamics can be matched with the LGM?
-ggplot(data=kanga1, mapping=aes(x = log(t_unscaled_low), y = pop_hist)) + geom_step(colour="red") + labs(xlab("log(years in past)"), ylab("population size")) + ggtitle("The red kangaroo's population dynamics, and the LGM") + geom_step(data = kanga1, mapping=aes(x=log(t_unscaled_hi), y=pop_hist), colour="black") + geom_vline(xintercept = log(LGM_end)) + geom_vline(xintercept = log(LGM_start))
+# let's see if the population dynamics can be matched with the LGM? LOG VER
+ggplot(data=kanga1, mapping=aes(x = log(t_unscaled_low, base=10), y = pop_hist)) + geom_step(colour="red") + labs(xlab("log10(years in past)"), ylab("population size")) + ggtitle("The red kangaroo's population dynamics, and the LGM") + geom_step(data = kanga1, mapping=aes(x=log(t_unscaled_hi, base=10), y=pop_hist), colour="black") + geom_vline(xintercept = log(LGM_end, base=10)) + geom_vline(xintercept = log(LGM_start, base=10))
+# let's see if the population dynamics can be matched with the LGM? UNLOGGED VER
+#ggplot(data=kanga1, mapping=aes(x = t_unscaled_low, y = pop_hist)) + geom_step(colour="red") + labs(xlab("years in past"), ylab("population size")) + ggtitle("The red kangaroo's population dynamics, and the LGM") + geom_step(data = kanga1, mapping=aes(x=t_unscaled_hi, y=pop_hist), colour="black") + geom_vline(xintercept = LGM_end) + geom_vline(xintercept = LGM_start)
